cc28de5a411bb0ea455f391703d9f3cf2bafebc8
Dvir Volk <dvirsky@gmail.com>
2017-07-03 17:14:11 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rsctl',
-    version='0.1',
+    version='0.2',
     description='Command-line control tool for RediSearch clusters',
     author='RedisLabs',
     url='https://github.com/redislabs/rsctl',
